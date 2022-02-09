0369072a7c7f7740f1f4a9e66bcee6645b64406d
Alec Ritson <hello@itsalec.co.uk>
2022-02-09 15:05:44 +0000

    Add media customisation

---
+++ app/Media/MediaPathResolver.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Media;
+
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class MediaPathResolver implements PathGenerator
+{
+    /**
+     * Get the root folder for the media relative to the model name.
+     *
+     * @param Media $media
+     * @return string
+     */
+    public function getRootMediaFolder($media)
+    {
+        return Str::plural(
+            strtolower(class_basename($media->model_type))
+        );
+    }
+
+    /*
+     * Get the path for the given media, relative to the root storage path.
+     */
+    public function getPath(Media $media): string
+    {
+        return $this->getRootMediaFolder($media) . '/' . $media->created_at->format('Y/m/d') . '/';
+    }
+
+    /*
+     * Get the path for conversions of the given media, relative to the root storage path.
+     */
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getRootMediaFolder($media) . '/' . $media->created_at->format('Y/m/d').'/conversions/';
+    }
+
+    /*
+     * Get the path for responsive images of the given media, relative to the root storage path.
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getRootMediaFolder($media) . '/' . $media->created_at->format('Y/m/d') .'/responsive-images/';
+    }
+}
