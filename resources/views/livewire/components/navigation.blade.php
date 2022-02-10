d52515c9dbaedfd6c6910672e156f2787fc83cc6
Alec Ritson <hello@itsalec.co.uk>
2022-02-10 12:21:21 +0000

    Remove account link

--- resources/views/livewire/components/navigation.blade.php
+++ resources/views/livewire/components/navigation.blade.php
@@ -51,25 +51,7 @@
       </form>
 
       <div class="flex items-center">
-        <div class="items-center hidden divide-x divide-gray-100 lg:flex">
-          <a href="" class="block px-6 text-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4 mx-auto"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
-            <span class="block mt-1 text-xs font-medium">Account</span>
-          </a>
-        </div>
+
 
         <div>
             @livewire('components.cart')
