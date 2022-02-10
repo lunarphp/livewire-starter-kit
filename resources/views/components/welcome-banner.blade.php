8741691e750324a183e8edccc739ac9154967c3d
markmead <itsmarkmead@gmail.com>
2022-02-10 17:04:32 +0000

    Re-use collection styling and include a welcome banner

---
+++ resources/views/components/welcome-banner.blade.php
@@ -0,0 +1,30 @@
+<section class="bg-gray-50">
+    <div class="max-w-screen-xl px-4 py-32 mx-auto">
+        <div class="max-w-xl mx-auto text-center">
+            <h1 class="text-3xl font-extrabold sm:text-5xl">
+                Welcome to the
+
+                <span class="text-blue-500">
+                    GetCandy
+                </span>
+
+                Demo Store
+
+                <span role="img">
+                    👋
+                </span>
+            </h1>
+
+            <p class="mt-4 font-medium sm:leading-relaxed sm:text-xl">
+                This example is a classic e-commerce store and we have a
+                <a
+                    class="text-blue-500 underline underline-offset-2"
+                    href="https://docs.getcandy.io/screencasts"
+                >
+                    screencast series in the docs
+                </a>
+                which show you how we created each section of the store.
+            </p>
+        </div>
+    </div>
+</section>
