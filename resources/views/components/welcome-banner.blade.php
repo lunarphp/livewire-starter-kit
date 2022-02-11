7bfc00d821028df1813133cf0b43ca9067513afc
Alec Ritson <hello@itsalec.co.uk>
2022-02-11 12:06:42 +0000

    Update welcome-banner.blade.php

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -16,16 +16,7 @@
             </h1>
 
             <p class="mt-4 font-medium sm:leading-relaxed sm:text-xl">
-                This example is a classic e-commerce store and we have a
-                <a
-                    class="text-blue-500 underline underline-offset-2"
-                    href="https://docs.getcandy.io/screencasts"
-                    target="_blank"
-                    rel="noopener"
-                >
-                    screencast series in the docs
-                </a>
-                which show you how we created each section of the store.
+                This example is a classic e-commerce store and we are currently making a screencast series to show you how we did it!
             </p>
         </div>
     </div>
