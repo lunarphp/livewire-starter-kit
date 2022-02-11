23a819afcbb965bf2975bde0b69004e1c93482ce
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-02-11 14:02:50 +0000

    Initial commit (#7)
    Co-authored-by: Alec Ritson <hello@itsalec.co.uk>

--- resources/views/livewire/collection-page.blade.php
+++ resources/views/livewire/collection-page.blade.php
@@ -6,33 +6,7 @@
 
         <div class="grid grid-cols-1 gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3">
             @forelse($this->collection->products as $product)
-                <a
-                    class="block group"
-                    href="{{ route('product.view', $product->defaultUrl->slug) }}"
-                >
-                    <div class="overflow-hidden rounded-lg aspect-w-1 aspect-h-1">
-                        @if ($product->thumbnail)
-                            <img
-                                class="object-cover transition-transform duration-300 group-hover:scale-105"
-                                src="{{ $product->thumbnail->getUrl('medium') }}"
-                                alt="{{ $product->translateAttribute('name') }}"
-                                loading="lazy"
-                            />
-                        @endif
-                    </div>
-
-                    <h5 class="mt-2 font-medium">
-                        {{ $product->translateAttribute('name') }}
-                    </h5>
-
-                    <p class="mt-1 text-sm text-gray-600">
-                        <span class="sr-only">
-                            Price
-                        </span>
-
-                        <x-product-price :product="$product" />
-                    </p>
-                </a>
+                <x-product-card :product="$product" />
             @empty
             @endforelse
         </div>
