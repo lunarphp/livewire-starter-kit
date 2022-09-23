e633cbc3c0ec8c5ef4936fb5a3fa519474cc50a2
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-09-23 16:46:09 +0100

    Hotfix - Freshen up dependencies (#48)
    * Remove JS
    
    * Bump yarn
    
    * Fresh packages
    
    * Fix css name

--- resources/views/livewire/components/add-to-cart.blade.php
+++ resources/views/livewire/components/add-to-cart.blade.php
@@ -6,7 +6,7 @@
                 Quantity
             </label>
 
-            <input class="w-16 px-1 py-4 text-sm text-center transition border border-gray-100 rounded-lg no-spinners"
+            <input class="w-16 px-1 py-4 text-sm text-center transition border border-gray-100 rounded-lg no-spinner"
                    type="number"
                    id="quantity"
                    min="1"
