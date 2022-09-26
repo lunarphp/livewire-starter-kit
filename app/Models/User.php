c0d55fd23893bbf2992b34150748c77b0494e504
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-09-26 10:40:47 +0100

    Hotfix - Remove meilisearch (#46)
    * Init
    
    * Update to new driver
    
    * Update scout config
    
    * Remove MySQL

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Lunar\Base\Traits\LunarUser;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
