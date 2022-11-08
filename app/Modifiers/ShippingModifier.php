d40d65c09446f972fcd51ff401c9b0763db97215
Aslam <aslam.hafidz@meteor.id>
2022-11-08 11:35:45 +0700

    Hotfix - Checkout Save Address

--- app/Modifiers/ShippingModifier.php
+++ app/Modifiers/ShippingModifier.php
@@ -17,6 +17,7 @@ class ShippingModifier
 
         ShippingManifest::addOption(
             new ShippingOption(
+                name: 'Basic Shipping',
                 description: 'Basic Delivery',
                 identifier: 'BASDEL',
                 price: new Price(500, $cart->currency, 1),
@@ -26,6 +27,7 @@ class ShippingModifier
 
         ShippingManifest::addOption(
             new ShippingOption(
+                name: 'Express Shipping',
                 description: 'Express Delivery',
                 identifier: 'EXPDEL',
                 price: new Price(1000, $cart->currency, 1),
