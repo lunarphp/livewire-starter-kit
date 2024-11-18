5461819dfb106e3cad2c615ec3314606e1b1390c
Yasir Turk <yasirmturk@gmail.com>
2024-11-18 01:40:14 +0100

    Styles aura logo

--- resources/views/components/brand/logo.blade.php
+++ resources/views/components/brand/logo.blade.php
@@ -1,7 +1,18 @@
 <svg {{ $attributes }}
      xmlns="http://www.w3.org/2000/svg"
      fill="none"
-     viewBox="0 0 521 326">
+     viewBox="0 0 1240 326" aria-labelledby=":PK786:" data-testid="aura-logo">
+    <title id=":PK786:">aura style</title>
+    <style>
+      .fashion {
+        font: 5.8em serif;
+      }
+      .aura {
+        font: bold 11.5em sans-serif;
+      }
+    </style>
     <path fill="currentColor"
           d="m221.63 36.43-.05.02C210.91 15.15 188.94.5 163.49.5c-25.43 0-47.4 14.64-58.07 35.93h-.07S7.19 232.72 6.27 234.85l-.11.23A64.999 64.999 0 001 261.74c.63 33.85 28.16 62.17 61.98 63.7C100.26 327.11 131 297.4 131 260.5c0-27.52-17.11-51.01-41.27-60.49-9.1-3.57-13.25-14.1-8.88-22.85l25.78-51.62c2.93-5.86 11.29-5.86 14.22 0 0 0 82.33 164.71 82.99 165.93v.01c12.04 22.16 36.58 36.53 64.15 33.65 30.61-3.19 55.19-28.4 57.77-59.07 1.15-13.69-2-26.55-8.17-37.48L221.63 36.43Z" />
+    <text x="380" y="132" class="fashion" fill="#111827">ONE STOP FASHION</text>
+    <text x="380" y="280" class="aura" fill="#111827">aura style</text>
 </svg>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-gray-50">
     <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
-        <x-brand.logo class="w-auto h-8 text-gray-500" />
+        <x-brand.logo class="w-auto h-16 text-gray-500" />
 
         <p class="max-w-sm mt-4 text-gray-700">
             This is a classic e-commerce store built with <a href="https://zabrdast.com">Zabrdast</a>.
